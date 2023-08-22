7ec075c0ee15065431c6e6cc08060f1c2bccc223
Aniket Pandharabale <aniketicloud@gmail.com>
2023-08-22 20:53:36 +0530

    changing the approach

--- src/components/NavList.tsx
+++
@@ -1,7 +0,0 @@
-export const NavList = () => {
-  return (
-    <>
-      <h1>NavList</h1>
-    </>
-  );
-};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,3 @@
-import { useState } from "react";
-import { LogoDetails } from "./LogoDetails";
-import { NavList } from "./NavList";
-
 export const SideBar = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  return (
-    <>
-      <div
-        className={`sidebar ${isExpanded && "open"}`}
-        onClick={() => setIsExpanded(!isExpanded)}
-      >
-        <LogoDetails isExpanded={isExpanded} />
-      </div>
-      <NavList />
-    </>
-  );
+  return <>SideBar</>;
 };
