9cc1bcf71abdaa2daadc24a8250e2e48e7da33ad
Aniket Pandharabale <aniketicloud@gmail.com>
2023-09-28 23:58:32 +0530

    footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="mt-auto">
+      <p className="text-xl text-gray-400 max-w-full text-center">
+        &copy; Copyright {new Date().getFullYear()} by Yash Technologies
+      </p>
+    </footer>
+  );
 };
