416c98000b02c02b8295932f127c48800efa5e1b
Aniket Pandharabale <aniketicloud@gmail.com>
2023-09-29 01:09:26 +0530

    using custom hook for width and height

--- src/layout/SideNav.tsx
+++ src/layout/SideNav.tsx
@@ -4,19 +4,21 @@ import { NavLink } from "react-router-dom";
 import { ComputerDesktop } from "../components/icons/ComputerDesktop";
 import { AcademicCap } from "../components/icons/AcademicCap";
 import "./Sidenav.css";
+import { useWindowDimensions } from "../hooks/useWindowDimension";
 
 interface SideNavProps {
   isExpanded: boolean;
 }
 
 export const SideNav: FC<SideNavProps> = ({ isExpanded }) => {
+  const { height, width } = useWindowDimensions();
   return (
     <nav className="bg-blue-400 px-3 py-4">
       <ul className="mt-4 space-y-2 font-medium">
         <li>
           <NavLink
             to="/"
-            className="flex items-center pt-2 text-gray-900 rounded-lg hover:bg-gray-100"
+            className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
           >
             <ComputerDesktop />
             {isExpanded && <span>Dashboard</span>}
@@ -26,13 +28,16 @@ export const SideNav: FC<SideNavProps> = ({ isExpanded }) => {
         <li>
           <NavLink
             to="/courses"
-            className="flex items-center pt-2 text-gray-900 rounded-lg hover:bg-gray-100"
+            className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
           >
             <AcademicCap />
             {isExpanded && <span>Courses</span>}
           </NavLink>
         </li>
       </ul>
+
+      <li>Height: {height}</li>
+      <li>width: {width}</li>
     </nav>
   );
 };
